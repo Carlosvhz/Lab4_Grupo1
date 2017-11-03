a11c1d91210bb1a47d922aedcbc2a61ded7342c0
Franklin Garcia <franklin__garcia@unitec.edu>
2017-11-03 16:03:42 -0600

    cc

--- minesweeper.cpp
+++ minesweeper.cpp
@@ -1,12 +1,31 @@
 #include "minesweeper.h"
+#include <cstdlib>
+#include <time.h>
+#include <math.h>
 
 minesweeper::minesweeper(int pn){
 	n = pn;
 	matriz = crearmatriz(n);
 }
 
+
 //Funcion crear matriz
 
 int** minesweeper::crearmatriz(int n){
-	//Crear matriz y llenarlas de minas	
+	srand(time(NULL));
+	int**matrix=new int*[n];
+	for(int i=0;i<n<i++){
+		matrix[i]=new char[n];
+	}
+	int i=0, j=0, k=rand()%9;
+
+	for(int h=0;h<k;h++){
+		i=rand()%i;
+		j=rand()%9;
+		matrix[i][j] = -1;
+	}
+	
+	
+	
+return matrix;
 }
